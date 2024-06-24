5279764b8feb749801c1bb6e0e19b75751290fba
HT3163 <hamzagujjar3163@gmail.com>
2024-06-24 20:56:43 +0500

    adding backend host url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // Replace with your backend URL
+  // baseURL: 'http://localhost:5000/api', // Replace with your backend URL
+  baseURL: 'https://work1-attendance-backend.vercel.app/api', // Replace with your backend URL
   headers: {
     'Content-Type': 'application/json',
   },
